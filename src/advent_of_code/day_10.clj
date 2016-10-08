e9e0d6ed6b226e96eb2afdd60dc085018bbae302
jrheard <jrheard@cs.stanford.edu>
2016-10-08 16:36:27 -0700

    WIP on day 10 part 1; solution is clean, but runs too slow / runs out of memory :'(

--- src/advent_of_code/day_10.clj
+++ src/advent_of_code/day_10.clj
@@ -2,19 +2,33 @@
   (:require [clojure.spec :as s]
             [clojure.string :refer [split]]))
 
+; http://adventofcode.com/2015/day/10
+
 (s/def ::digit (s/and nat-int? #(< % 10)))
 (s/def ::run (s/& (s/+ ::digit)
-                    #(= (count (set %)) 1)))
+                  #(= (count (set %)) 1)))
 (s/def ::sequence (s/* ::run))
 
 (def start [1])
 
 (defn look-and-say [look-and-say-seq]
-
-  )
+  (println (count look-and-say-seq))
+  (let [parsed-sequence (s/conform ::sequence look-and-say-seq)]
+    (mapcat (fn [run]
+              [(count run) (first run)])
+            parsed-sequence)))
 
 (comment
+  (-> look-and-say
+      (iterate start)
+      (nth 20)
+      count)
+
   (s/exercise ::run)
 
-  (s/conform ::sequence [1 1 2 2 2 2 1 3 4 4 4 2])
+  (s/conform ::sequence [1])
+
+  (look-and-say start)
+
+  (take 40 (iterate look-and-say start))
   )
