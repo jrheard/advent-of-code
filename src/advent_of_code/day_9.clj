5775cea3ea393b8f74e929ed6d1b8773adb0318e
jrheard <jrheard@cs.stanford.edu>
2016-10-08 15:33:32 -0700

    day 9 part 1

--- src/advent_of_code/day_9.clj
+++ src/advent_of_code/day_9.clj
@@ -1,6 +1,8 @@
 (ns advent-of-code.day-9
   (:require [clojure.spec :as s]
             [clojure.edn :as edn]
+            [clojure.math.combinatorics :refer [permutations]]
+            [clojure.set :refer [union]]
             [clojure.string :refer [split]]))
 
 ; traveling santa problem
@@ -13,26 +15,59 @@
                         :equals #{"="}
                         :cost ::distance))
 
-(def raw-inputs "London to Dublin = 464\nLondon to Belfast = 518\nDublin to Belfast = 141")
+(def raw-inputs "Faerun to Norrath = 129\nFaerun to Tristram = 58\nFaerun to AlphaCentauri = 13\nFaerun to Arbre = 24\nFaerun to Snowdin = 60\nFaerun to Tambi = 71\nFaerun to Straylight = 67\nNorrath to Tristram = 142\nNorrath to AlphaCentauri = 15\nNorrath to Arbre = 135\nNorrath to Snowdin = 75\nNorrath to Tambi = 82\nNorrath to Straylight = 54\nTristram to AlphaCentauri = 118\nTristram to Arbre = 122\nTristram to Snowdin = 103\nTristram to Tambi = 49\nTristram to Straylight = 97\nAlphaCentauri to Arbre = 116\nAlphaCentauri to Snowdin = 12\nAlphaCentauri to Tambi = 18\nAlphaCentauri to Straylight = 91\nArbre to Snowdin = 129\nArbre to Tambi = 53\nArbre to Straylight = 40\nSnowdin to Tambi = 15\nSnowdin to Straylight = 99\nTambi to Straylight = 70")
 (def inputs (split raw-inputs #"\n"))
 (def segments (map (fn [input]
                      (s/conform ::segment
                                 (split input #" ")))
                    inputs))
 
-(def cost-map (into {}
-                    (map (fn [segment]
-                           [#{(segment :origin) (segment :destination)}
-                            (segment :cost)])
-                         segments)))
+(def cost-map
+  (into {}
+        (map (fn [segment]
+               [#{(segment :origin) (segment :destination)}
+                (segment :cost)])
+             segments)))
 
+(def all-locations
+  (apply union (keys cost-map)))
+
+(defn route-cost [route]
+  (reduce (fn [state curr-location]
+            {:last-location curr-location
+             :cost          (+ (state :cost)
+                               (cost-map #{(state :last-location) curr-location}))})
+          {:last-location (first route)
+           :cost          0}
+          (rest route)))
+
+(def all-routes
+  (into {}
+        (map (fn [route]
+               [route (route-cost route)])
+             (permutations all-locations))))
+
+(def min-cost-route
+  (apply min-key
+         (fn [[_ state]]
+           (state :cost))
+         all-routes))
 
 (comment
+  all-routes
+
+  min-cost-route
+
+  (rest all-locations)
   (as-> "London to Dublin = 464" $
         (split $ #" ")
         (s/conform ::segment $)
         )
 
+  (permutations all-locations)
+
   cost-map
+
+  all-locations
   )
 
