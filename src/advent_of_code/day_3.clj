9b48c3eee6b9d7b0781eda4349f8113bfb1290e8
jrheard <jrheard@cs.stanford.edu>
2016-10-07 17:24:26 -0700

    day 3

--- src/advent_of_code/day_3.clj
+++ src/advent_of_code/day_3.clj
@@ -1,5 +1,6 @@
 (ns advent-of-code.day-3
-  (:require [clojure.spec :as s]))
+  (:require [clojure.spec :as s]
+            [clojure.spec.test :as stest]))
 
 ; http://adventofcode.com/2015/day/3
 
@@ -13,40 +14,78 @@
 (s/def ::location ::house)
 (s/def ::state (s/keys ::visited-counts ::location))
 
-(def night-trip-info
-  (reduce
-    (fn [state direction]
-      (let [location (state ::location)
-            new-location (case direction
-                           \^ {::x (location ::x)
-                               ::y (inc (location ::y))}
-                           \v {::x (location ::x)
-                               ::y (dec (location ::y))}
-                           \> {::x (inc (location ::x))
-                               ::y (location ::y)}
-                           \< {::x (dec (location ::x))
-                               ::y (location ::y)})
-            new-state (assoc state ::location new-location)]
-        (if (contains? (state ::visited-counts) new-location)
-          (update-in new-state [::visited-counts new-location] inc)
-          (update-in new-state [::visited-counts] assoc new-location 1))))
+(def starting-state {::location       {::x 0 ::y 0}
+                     ::visited-counts {{::x 0 ::y 0} 1}})
 
-    {::location       {::x 0 ::y 0}
-     ::visited-counts {{::x 0 ::y 0} 1}}
+(defn handle-direction [state direction]
+  (let [location (state ::location)
+        new-location (case direction
+                       \^ {::x (location ::x)
+                           ::y (inc (location ::y))}
+                       \v {::x (location ::x)
+                           ::y (dec (location ::y))}
+                       \> {::x (inc (location ::x))
+                           ::y (location ::y)}
+                       \< {::x (dec (location ::x))
+                           ::y (location ::y)})
+        new-state (assoc state ::location new-location)]
+    (if (contains? (state ::visited-counts) new-location)
+      (update-in new-state [::visited-counts new-location] inc)
+      (update-in new-state [::visited-counts] assoc new-location 1))))
 
-    input))
+(s/fdef handle-direction
+  :args (s/cat :state ::state
+               :direction char?)
+  :ret ::state)
 
-(def part-1
+(def part-1-santa-state (reduce handle-direction starting-state input))
+
+(defn count-visited-houses [visited-counts]
   (count
     (filter #(> % 0)
-            (vals (night-trip-info ::visited-counts)))))
+            (vals visited-counts))))
+
+(s/fdef count-visited-houses
+  :args (s/cat :visited-counts ::visited-counts)
+  :ret int?)
+
+(def part-1 (count-visited-houses (part-1-santa-state ::visited-counts)))
+
+(def santa-indexes (filter even? (range (count input))))
+(def robo-santa-indexes (filter odd? (range (count input))))
+
+(def part-2-santa-state
+  (reduce handle-direction
+          starting-state
+          (map #(nth input %) santa-indexes)))
+
+(def part-2-robo-santa-state
+  (reduce handle-direction
+          starting-state
+          (map #(nth input %) robo-santa-indexes)))
+
+(def part-2
+  (count-visited-houses
+    (merge-with +
+                (part-2-santa-state ::visited-counts)
+                (part-2-robo-santa-state ::visited-counts))))
+
+(stest/instrument `handle-direction
+                  `count-visited-houses)
 
 (comment
+  (count input)
+  (last (range (count input)))
+
   (vals (night-trip-info ::visited-counts))
 
   (filter #(> % 0)
-            (vals (night-trip-info ::visited-counts)))
+          (vals (night-trip-info ::visited-counts)))
+
+  (merge-with +
+              (part-2-santa-state ::visited-counts)
+              (part-2-robo-santa-state ::visited-counts))
 
   part-1
-
+  part-2
   )
