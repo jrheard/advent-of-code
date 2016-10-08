38cb6e4a6e73c5b78dd25b9c1e34e0840cf77a44
jrheard <jrheard@cs.stanford.edu>
2016-10-08 16:48:19 -0700

    implement faster version of day 10 part 1

--- src/advent_of_code/day_10.clj
+++ src/advent_of_code/day_10.clj
@@ -4,25 +4,57 @@
 
 ; http://adventofcode.com/2015/day/10
 
+(def start [1 3 2 1 1 3 1 1 1 2])
+
+; super pretty look-and-say implementation with spec; too slow though :'(
+
 (s/def ::digit (s/and nat-int? #(< % 10)))
 (s/def ::run (s/& (s/+ ::digit)
                   #(= (count (set %)) 1)))
 (s/def ::sequence (s/* ::run))
 
-(def start [1])
-
-(defn look-and-say [look-and-say-seq]
+(defn look-and-say
+  [look-and-say-seq]
   (println (count look-and-say-seq))
   (let [parsed-sequence (s/conform ::sequence look-and-say-seq)]
     (mapcat (fn [run]
               [(count run) (first run)])
             parsed-sequence)))
 
+; faster implementation with reduce
+
+(defn fast-look-and-say
+  [look-and-say-seq]
+  (println (count look-and-say-seq))
+  (let [unprocessed-state (reduce (fn [state digit]
+                                    (if (= digit (state :run-digit))
+                                      (update-in state [:run-count] inc)
+
+                                      (-> state
+                                          (update-in [:new-seq]
+                                                     conj
+                                                     (state :run-count)
+                                                     (state :run-digit))
+                                          (assoc :run-count 1)
+                                          (assoc :run-digit digit))))
+                                  {:new-seq   []
+                                   :run-count 1
+                                   :run-digit (first look-and-say-seq)}
+                                  (rest look-and-say-seq))]
+    (conj (unprocessed-state :new-seq)
+          (unprocessed-state :run-count)
+          (unprocessed-state :run-digit))))
+
 (comment
-  (-> look-and-say
+  (fast-look-and-say (fast-look-and-say start))
+
+  (-> fast-look-and-say
       (iterate start)
-      (nth 20)
-      count)
+      (nth 40)
+      count
+      )
+
+  (conj [2 3 4] 1 2)
 
   (s/exercise ::run)
 
